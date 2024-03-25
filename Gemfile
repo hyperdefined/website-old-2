04e8f073f74f54f2ca824fb4d7968e4a3ed8ee74
hyperdefined <contact@hyper.lol>
2024-03-24 22:31:36 -0400

    various improvements

--- Gemfile
+++ Gemfile
@@ -8,6 +8,8 @@ source "https://rubygems.org"
 # This will help ensure the proper Jekyll version is running.
 # Happy Jekylling!
 gem "jekyll", "~> 4.3.2"
+gem "kramdown"
+gem "rouge"
 # If you want to use GitHub Pages, remove the "gem "jekyll"" above and
 # uncomment the line below. To upgrade, run `bundle update github-pages`.
 # gem "github-pages", group: :jekyll_plugins
@@ -16,7 +18,6 @@ group :jekyll_plugins do
   gem "jekyll-paginate-v2"
   gem "jekyll-sitemap"
 end
-
 # Windows and JRuby does not include zoneinfo files, so bundle the tzinfo-data gem
 # and associated library.
 platforms :mingw, :x64_mingw, :mswin, :jruby do
--- _plugins/reading_time_filter.rb
+++ _plugins/reading_time_filter.rb
@@ -1,13 +1,13 @@
 module ReadingTimeFilter
     include Liquid::StandardFilters
-  
+
     def reading_time(input)
       # Get words count.
       total_words = get_plain_text(input).split.size
-  
+
       # Load configuration.
       config = @context.registers[:site].config["reading_time"]
-  
+
       # Setup default value.
       if ! config
         second_plural = "seconds"
@@ -18,10 +18,10 @@ module ReadingTimeFilter
         minute_singular = config["minute_singular"] ? config["minute_singular"] : "minute"
         minute_plural = config["minute_plural"] ? config["minute_plural"] : "minutes"
       end
-  
+
       # Average reading words per minute.
       words_per_minute = 180
-  
+
       # Calculate reading time.
       case total_words
       when 0 .. 89
@@ -41,15 +41,15 @@ module ReadingTimeFilter
         return "#{minutes} #{minute_plural}";
       end
     end
-  
+
     def get_plain_text(input)
       strip_html(strip_pre_tags(input))
     end
-  
+
     def strip_pre_tags(input)
       empty = ''.freeze
       input.to_s.gsub(/<pre(?:(?!<\/pre).|\s)*<\/pre>/mi, empty)
     end
   end
-  
+
   Liquid::Template.register_filter(ReadingTimeFilter)
